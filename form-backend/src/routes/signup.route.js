e1ca17d10a29929085d78c5e0b2d2bca1d0fd9d0
VivekAnalah <vivek.belwal@analah.in>
2023-12-22 12:12:02 +0530

    changes in login route

--- form-backend/src/routes/signup.route.js
+++ form-backend/src/routes/signup.route.js
@@ -38,8 +38,9 @@ app.post("/login", async (req, res) => {
         console.log(user)
         const remainingTimeInSeconds = user.remainingTime;
         const name = user.name;
-        if(user.exam){
-
+      
+        if(user.Exam){
+          console.log("Backend", user.exam, user.points,user.date)
           return res.send({Status : "Ok", msg:"login successful", email: email, remainingTimeInSeconds, name, exam: user.Exam, points: user.points, date:user.date });
         }
         return res.send({Status : "Ok", msg:"login successful", email: email, remainingTimeInSeconds, name, exam: user.Exam});
